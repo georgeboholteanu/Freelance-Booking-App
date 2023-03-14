6c53dcada231f15c26e60e94ba84367399060221
DavidHJolley <davidhjolley2000@gmail.com>
2023-03-14 15:53:15 -0400

    Initial Commit
    Adds navbar functionality

---
+++ client/src/components/NavTabs.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function NavTabs() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="about"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          About
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="adduser"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Add User
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
