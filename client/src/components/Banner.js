b1eced332fa657ea4b56cf0938d672a5812dcd0c
georgeboholteanu <george.boholteanu@gmail.com>
2023-03-17 21:32:30 +0000

    banner styling

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import About from "./pages/About";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 function Banner() {
   const [isVisible, setIsVisible] = useState(true);
@@ -15,40 +13,35 @@ function Banner() {
   }
 
   return (
-    <section class="bg-gray-50">
-      <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-        <div class="mx-auto max-w-xl text-center">
-          <h1 class="text-3xl font-extrabold sm:text-5xl">
+    <section className="bg-gray-50">
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
             Support Freelancers
-            <strong class="font-extrabold text-red-700 sm:block">
+            <strong className="font-extrabold text-red-700 sm:block">
               Increase productivity
             </strong>
           </h1>
 
-          <p class="mt-4 sm:text-xl sm:leading-relaxed">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
-            tenetur fuga ducimus numquam ea!
+          <p className="mt-4 sm:text-xl sm:leading-relaxed">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+            illo tenetur fuga ducimus numquam ea!
           </p>
 
-          <div class="mt-8 flex flex-wrap justify-center gap-4">
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
             <button
-              class="block w-full rounded px-12 py-3 text-sm font-medium text-white bg-red-600 shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              className="block w-full rounded px-12 py-3 text-sm font-medium text-white bg-red-600 shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
               onClick={handleHireNowClick}
             >
               Hire now!
             </button>
 
-            <a
-              class="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
-              href="./about"
+            <NavLink
+              to="./about"
+              className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
             >
               About
-              <NavLink
-                to="./about"
-                className=""
-                activeClassName="border-b-2 font-semibold text-gray-800"
-              ></NavLink>
-            </a>
+            </NavLink>
           </div>
         </div>
       </div>
