fd29c24bd71738a22bd079069af7eec658883747
David J <davidhjolley2000@gmail.com>
2023-03-17 15:16:45 -0400

    Adds functionality for hero and routing improvements

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import Users from '../Users';
 import Banner from '../Banner';
-import Dropdown from '../Filter'
+import Dropdown from '../Filter';
 
 function Home () {
   const cities = ["London", "Manchester", "Birmingham", "Newcastle"];
   const skills = ["HTML", "CSS", "Javascript", "React"];
-  const availability = ["Full-time", "Part-time", "Contract", "Available", ];
+  const availability = ["Full-time", "Part-time", "Contract", "Available"];
   
   return (
     <div>
-        <br/>
-        <Banner />
-        <br/>
-        <div className="flex justify-center gap-6">
-          <Dropdown options={cities} label="Select City" />
-          <Dropdown options={skills} label="Select Skills" />
-          <Dropdown options={availability} label="Select Availability" />
-        </div>
-        <Users />        
+      <Banner />
+      <br />
+      <div className="flex justify-center gap-6">
+        <Dropdown options={cities} label="Select City" />
+        <Dropdown options={skills} label="Select Skills" />
+        <Dropdown options={availability} label="Select Availability" />
+      </div>
+      <Users />        
     </div>
-  )
+  );
 }
+
 export default Home;
