478c4aa177444fee9d7899b6546bbac8a3c35331
David J <davidhjolley2000@gmail.com>
2023-03-21 15:56:28 -0400

    Styling additions
    Added styling to navbar and about page

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -1,43 +1,45 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaHome, FaInfoCircle, FaUserPlus, FaEnvelope } from 'react-icons/fa';
 
 function NavTabs() {
   return (
     <ul className="flex border-b-2 border-gray-200">
       <li className="mr-1">
         <NavLink
-              to="/"
-              onClick={() => window.reload()}
-              className="inline-block py-2 px-4 text-gray-500 hover:text-gray-800 font-semibold"
-            >
-        Home
+          to="/"
+          onClick={() => window.reload()}
+          className="inline-block py-2 px-4 text-gray-500 hover:text-gray-800 font-semibold flex items-center"
+        >
+          <FaHome className="mr-2" />
+          Home
         </NavLink>
       </li>
       <li className="mr-1">
         <NavLink
-              to="/about"
-              className="inline-block py-2 px-4 text-gray-500 hover:text-gray-800 font-semibold"
-              
-            >
-        About
+          to="/about"
+          className="inline-block py-2 px-4 text-gray-500 hover:text-gray-800 font-semibold flex items-center"
+        >
+          <FaInfoCircle className="mr-2" />
+          About
         </NavLink>
       </li>
       <li className="mr-1">
         <NavLink
-              to="/adduser"
-              className="inline-block py-2 px-4 text-gray-500 hover:text-gray-800 font-semibold"
-              
-            >
-        Add Freelancer
+          to="/adduser"
+          className="inline-block py-2 px-4 text-gray-500 hover:text-gray-800 font-semibold flex items-center"
+        >
+          <FaUserPlus className="mr-2" />
+          Add Freelancer
         </NavLink>
       </li>
       <li className="mr-1">
         <NavLink
-              to="/contact"
-              className="inline-block py-2 px-4 text-gray-500 hover:text-gray-800 font-semibold"
-              
-            >
-        Contact
+          to="/contact"
+          className="inline-block py-2 px-4 text-gray-500 hover:text-gray-800 font-semibold flex items-center"
+        >
+          <FaEnvelope className="mr-2" />
+          Contact
         </NavLink>
       </li>
     </ul>
