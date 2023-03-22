c3046e562a65230c689164c50c0b8142373a870d
georgeboholteanu <george.boholteanu@gmail.com>
2023-03-22 23:58:37 +0000

    test

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.post("/adduser", async (req, res) => {
 });
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join("client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   })
