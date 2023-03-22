f76bc22bc11bec936f31c8a104428c587696add1
georgeboholteanu <george.boholteanu@gmail.com>
2023-03-22 23:48:53 +0000

    starter4

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.post("/adduser", async (req, res) => {
 });
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join("client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   })
@@ -111,5 +111,5 @@ if(process.env.NODE_ENV === "production") {
 }
 
 app.listen(process.env.PORT || 6000, () => {
-  console.log(`Server Started on port 5000`);
+  console.log(`Server Started on port 6000`);
 });
